f877c368c81ca3f30ce632d6584c86b5aa27bbe6
Leonhard Breuer <git@leonhardbreuer.de>
2026-01-05 16:44:06 +0100

    feat: add waving hand logo

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -67,6 +67,10 @@ function RootDocument({ children }: { children: React.ReactNode }) {
     <html lang="en">
       <head>
         <HeadContent />
+        <link
+                  rel="icon"
+                  href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>👋</text></svg>"
+                />
       </head>
       <body className="overflow-x-hidden overflow-y-hidden">
         {children}
