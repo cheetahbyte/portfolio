a2dd32422424f344b32a709baec8ebad9b7a734d
Leonhard Breuer <git@leonhardbreuer.de>
2026-06-15 20:33:29 +0200

    feat: nextjs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,47 +1,63 @@
-// src/app/page.tsx
-import MeCard from "@/components/Me";
-import ProfileImageCard from "@/components/ProfileImageCard";
-import SocialsCard from "@/components/SocialsCard";
-import LocationCard from "@/components/LocationCard";
-import LocalTimeCard from "@/components/LocalTimeCard";
-import StackCard from "@/components/StackCard";
-import StatCard from "@/components/StatCard";
-import LightSwitchCard from "@/components/LightSwitch";
-import LightSource from "@/components/LightSource";
-import AgeCard from "@/components/AgeCard";
-import ContactCard from "@/components/ContactCard";
-import ProjectsCard from "@/components/ProjectsCard";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="min-h-screen w-full relative overflow-hidden flex items-center justify-center">
-      {/* Background */}
-      <LightSource/>
-
-      {/* Bento container */}
-      <main className="w-full md:w-[65%] max-w-[1200px] rounded-3xl p-6" style={{ backgroundColor: "#0A0A0A" }}>
-        <div className="grid grid-cols-2 md:grid-cols-5 auto-rows-[160px] gap-4">
-          {/* Row 1 */}
-          <MeCard />
-          <ProfileImageCard/>
-          <SocialsCard/>
-           <LocalTimeCard />
-          
-          {/* Socials as tall right column */}
-          
-
-          {/* Row 2 */}
-          <LocationCard  />
-          
-          <AgeCard/>
-          <StatCard/>
-          <StackCard/>
-          
-
-          {/* Row 3 */}
-          <ProjectsCard/>
-          <LightSwitchCard/>
-          <ContactCard/>
+    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+        <Image
+          className="dark:invert"
+          src="/next.svg"
+          alt="Next.js logo"
+          width={100}
+          height={20}
+          priority
+        />
+        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
+          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
+            To get started, edit the page.tsx file.
+          </h1>
+          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
+            Looking for a starting point or more instructions? Head over to{" "}
+            <a
+              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+              className="font-medium text-zinc-950 dark:text-zinc-50"
+            >
+              Templates
+            </a>{" "}
+            or the{" "}
+            <a
+              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+              className="font-medium text-zinc-950 dark:text-zinc-50"
+            >
+              Learning
+            </a>{" "}
+            center.
+          </p>
+        </div>
+        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
+          <a
+            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
+            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              className="dark:invert"
+              src="/vercel.svg"
+              alt="Vercel logomark"
+              width={16}
+              height={16}
+            />
+            Deploy Now
+          </a>
+          <a
+            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
+            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Documentation
+          </a>
         </div>
       </main>
     </div>
