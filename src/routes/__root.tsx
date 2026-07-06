782abeaa166cadd97b4214d95cb3fd820318be2f
root <root@vmd200737.contaboserver.net>
2026-07-06 13:51:39 +0200

    fix: remove honeypot script

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -64,11 +64,6 @@ function RootDocument({ children }: { children: React.ReactNode }) {
 		<html lang="en">
 			<head>
 				<HeadContent />
-				<script
-					src="https://cupcake.orbiq.one/api/script.js"
-					data-site-id="1"
-					defer
-				></script>
 				<link
 					rel="icon"
 					href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>👋</text></svg>"
