6960805bc1bdfd022fbaf7f3a16b6a83d238303f
Leonhard Breuer <git@leonhardbreuer.de>
2026-08-18 19:16:21 +0200

    chore: Remove portfolio project files

--- src/components/StackItem.tsx
+++
@@ -1,7 +0,0 @@
-export function StackItem({ technology }: { technology: string }) {
-  return (
-    <li className="rounded-full border border-neutral-200 px-2.5 py-1 text-xs text-neutral-600 dark:border-neutral-700 dark:text-neutral-400">
-      {technology}
-    </li>
-  );
-}
