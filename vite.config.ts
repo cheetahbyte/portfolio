7cc594d29c73abda9169e0d095efc55f6d2832a5
Leonhard Breuer <git@leonhardbreuer.de>
2026-08-25 11:39:19 +0200

    chore(config): remove Cloudflare Vite plugin setup

--- vite.config.ts
+++ vite.config.ts
@@ -5,13 +5,11 @@ import tailwindcss from '@tailwindcss/vite'
 import { tanstackStart } from '@tanstack/solid-start/plugin/vite'
 
 import solidPlugin from 'vite-plugin-solid'
-import { cloudflare } from '@cloudflare/vite-plugin'
 
 export default defineConfig({
   resolve: { tsconfigPaths: true },
   plugins: [
     devtools(),
-    cloudflare({ viteEnvironment: { name: 'ssr' } }),
     tailwindcss(),
     tanstackStart({
       prerender: {
